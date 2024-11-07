263a661bc33b8aa01959156cec1fb81b5484784a
wopigjwoigjweroi <agizekel@gmail.com>
2024-11-06 20:54:01 -0800

    Update Lab29.cpp

--- Lab29.cpp
+++ Lab29.cpp
@@ -13,6 +13,28 @@ void populateChange(map<string,array<list<string>, 3>>& map, int time);
 // Define function for population change
 void populateChange(map<string,array<list<string>, 3>>& map, int time) {
 
-  
+    for (int i = 0; i < time; ++i) {
+
+        cout << "Time Period: " << i + 1 << endl; 
+
+    }
+
+    for (auto& part : map) {
+
+        string mapName = part.first;
+
+        auto& list = part.second; 
+
+        list[0].push_back("New Tree"); 
+
+        if (!list[0].empty()) {
+
+          list[1].push_back("Newborn Rabbit"); 
+        }
+
+        
+    }
+
+    
 
 }; 
