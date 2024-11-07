48220f2856482df61ed1a836bd0bf295de6fe5d7
wopigjwoigjweroi <agizekel@gmail.com>
2024-11-06 20:48:28 -0800

    Update Lab29.cpp

--- Lab29.cpp
+++ Lab29.cpp
@@ -5,3 +5,14 @@
 #include <array>
 #include <fstream>
 
+using namespace std; 
+
+// Define function prototype 
+void populateChange(map<string,array<list<string>, 3>>& map, int time);
+
+// Define function for population change
+void populateChange(map<string,array<list<string>, 3>>& map, int time) {
+
+  
+
+}; 
